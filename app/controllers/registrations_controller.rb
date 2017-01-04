64ce1b51ffe453161f434dfdfb33c2fb92b7355a
Sam Lau <samlau95@gmail.com>
2017-01-04 01:59:35 -0800

    WIP

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,11 +1,23 @@
 # frozen_string_literal: true
 class RegistrationsController < ApplicationController
   def new
+    @registration = Registration.new
   end
 
   def create
+    @registration = Registration.new registration_params
+
+    binding.pry
   end
 
   def show
+  end
+
+  private
+
+  def registration_params
+    params.require(:registration).permit(
+      :first_name, :last_name, :sid, :email, :github_username
+    )
   end
 end
